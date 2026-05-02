9d5c310e5358b5cc73fc168ad7bee59dfee65f6a
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-05-02 12:01:45 -0500

    Add files via upload

--- assembler.sh
+++ assembler.sh
@@ -2,12 +2,18 @@
 
 fileName="${1%%.*}" # remove .s extension
 
+# Assemble
 nasm -f elf64 ${fileName}".s"
+if [ $? -ne 0 ]; then
+  echo "NASM assembly failed" >&2
+  exit 1
+fi
+
 ld ${fileName}".o" -o ${fileName} -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2
 if [ "$2" == "-c" ]; then
 	exit
 elif [ "$2" == "-g" ]; then
-	gdb -q ${fileName}
+	gdb -q ${fileName} # Debug
 else
-	./${fileName}
+	./${fileName} # Execute
 fi
